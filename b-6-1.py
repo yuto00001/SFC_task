180ea57d1aa02fdfa5e710ab7106d90533380721
YUTO TANISHIKI <yutotanishiki@TANISHIKI.local>
2022-06-03 20:44:55 +0900

    b-6-1継続

--- b-6-1.py
+++ b-6-1.py
@@ -11,9 +11,11 @@
 # ---------------------------------------------------------
 # プログラムが最後まで実行されたのちに、再び最初から実行される処理が必要。
 # [想定1] while文によって図形の移動を可能にし、その処理自体をfor文で囲み,ひとつの処理として捉え,繰り返す。
+
 # [想定2] while文によって図形の移動を可能にし、その処理自体をもういちどwhile文で囲み,繰り返しを可能にする条件のもと,無限に繰り返す。
 
-# [想定3] シンプルに、pyxel.run(update, draw)を無限に繰り返す。
+# [想定3] if文を使って、aが200を超えたら処理を中断し、aを-200する
+
 # ---------------------------------------------------------
 # 無限ループをどう扱うか。ー 扱わない。
 # 定義関数(def関数)をどう活かすか。　ー　既に活用済み。
@@ -22,6 +24,22 @@
 # 無限ループのwhile文をきちんと終わらせ、その処理をさらにwhile文で囲うことで、二重構文とする。
 
 
+# import pyxel
+# pyxel.init(200,200)
+# a = 0
+
+# def update():
+#     global a
+#     a += 1
+
+# def draw():
+#     global a
+#     pyxel.cls(7)
+#     pyxel.circ(100+a, 100+a, 10, 0)
+
+# pyxel.run(update, draw)
+
+# if文を使って、aが200を超えたら処理を中断し、aを-200する。という方法を考えたが、pyxel.runがaの総数を取得していない(取得しているのはdef関数)ため、命令が効かないという段階。
 import pyxel
 pyxel.init(200,200)
 a = 0
@@ -33,9 +51,12 @@ def update():
 def draw():
     global a
     pyxel.cls(7)
-    pyxel.circ(a, a, 10, 0)
+    pyxel.circ(100+a, 100+a, 10, 0)
 
-pyxel.run(update, draw)
+if a<= 200:
+    pyxel.run(update, draw)
+else:
+    a - 200
 
 
 # import pyxel
@@ -50,6 +71,22 @@ pyxel.run(update, draw)
 #     pyxel.flip()
 #     a += 1
 
+# ボツ--------------
+# import pyxel
+
+# pyxel.init(200,200)
+
+# a = 0
+
+# while True:
+#     if a >= 200:
+#         pyxel.cls(7)
+#         pyxel.circ(a, a, 10, 0)
+#         pyxel.flip()
+#         a += 1
+#     else:
+#         break
+
 # --
 
 # import pyxel
@@ -58,7 +95,7 @@ pyxel.run(update, draw)
 
 # for i in range(10):
 #     while True:
-#         if a <= 200:
+#         if a <= 100:
 #             def update():
 #                 global a
 #                 a += 1
