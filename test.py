8af561fead88f77c533400db88836fc68fd6ee0f
YUTO TANISHIKI <yutotanishiki@TANISHIKI.local>
2022-06-05 20:40:47 +0900

    commit

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-def plus_three(x):
-  x += 3
-  print(x)
+import pyxel
+
+pyxel.init(200,200)
+
+a = 0
+
+def update():
+    global a
+    a += 1
+    if a > 200:
+        a = 0
+
+def draw():
+    global a
+    pyxel.cls(7)
+    pyxel.circ(a, a, 10, 0)
+
+
+pyxel.run(update, draw)
